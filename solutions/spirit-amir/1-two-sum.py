e577884121380ed6d1e24bd67359272f416a0a65
A. Aashique Khan <khanaashiqueamir@gmail.com>
2025-08-04 12:38:55 +0000

    feat: testing solution

---
+++ solutions/spirit-amir/1-two-sum.py
@@ -0,0 +1,16 @@
+def find_two_sum(nums, target):
+    num_dict = {}
+    for index, num in enumerate(nums):
+        complement = target - num
+        if complement in num_dict:
+            return [num_dict[complement], index]
+        num_dict[num] = index
+    return []
+
+# Example usage:
+
+target = int(input().strip())
+nums = list(map(int, input().strip().split()))
+
+result = find_two_sum(nums, target)
+print(result[0], result[1])
